15d201bd355153145d3cf2a87c09c5b6b074d962
Yuriy Kolerov <ykolerov@synopsys.com>
2025-05-01 04:19:37 -0700

    picocrt/arcv: Check ARC-V build CSR for setting mstatus.V
    misa.V bit is set only when full V extension is present.
    However, if a target implements only a subset of V then this
    bit is not set and we cannot rely on it for determining
    whether we need to set mstatus.VS bits.
    
    To overcome this limitation we can use ARC-V build register
    for RVV features. If its first 3 bits are not 0, then
    vector extensions are present.
    
    Signed-off-by: Yuriy Kolerov <ykolerov@synopsys.com>

--- picocrt/machine/riscv/crt0.c
+++ picocrt/machine/riscv/crt0.c
@@ -174,23 +174,59 @@ _trap(void)
 #endif
 
 #if defined(CRT0_ARCV) && !defined(CRT0_NO_CSR)
+#define CSR_NUM_MSTATUS                     0x300
+#define MSTATUS_VS_OFFSET                   0x9
+#define MSTATUS_VS_MASK                     0x3
+
 #define CSR_NUM_ARCV_MCACHE_CTRL            0x7C8
 #define ARCV_MCACHE_CTRL_IC_EN_OFFSET       0x0
 #define ARCV_MCACHE_CTRL_DC_EN_OFFSET       0x8
 #define ARCV_MCACHE_CTRL_DC_L0_EN_OFFSET    0xC
 #define ARCV_MCACHE_CTRL_L2_EN_OFFSET       0x10
 
+#define CSR_NUM_ARCV_RVV_BUILD              0xFC4
+#define ARCV_RVV_BUILD_V_OPTION_OFFSET      0x0
+#define ARCV_RVV_BUILD_V_OPTION_MASK        0x7
+
+inline unsigned long __attribute__((always_inline,target("arch=+zicsr")))
+_arcv_csr_read(int csr_num)
+{
+        unsigned long result;
+        __asm__ __volatile__("csrr %0, %1" : "=r"((result)) : "i"((csr_num)));
+        return result;
+}
+
+inline void __attribute__((always_inline,target("arch=+zicsr")))
+_arcv_csr_write(int csr_num, unsigned long data)
+{
+        __asm__ __volatile__("csrw %0, %1" :: "i"(csr_num), "r"(data));
+}
+
 static void __section(".init") __used  __attribute__((target("arch=+zicsr")))
 _arcv_cache_enable()
 {
-        unsigned long mcache;
-        __asm__ __volatile__("csrr %0, %1" : "=r"((mcache)) : "i"((CSR_NUM_ARCV_MCACHE_CTRL)));
+        unsigned long mcache = _arcv_csr_read(CSR_NUM_ARCV_MCACHE_CTRL);
         mcache |=
                 (1 << ARCV_MCACHE_CTRL_IC_EN_OFFSET) |
                 (1 << ARCV_MCACHE_CTRL_DC_EN_OFFSET) |
                 (1 << ARCV_MCACHE_CTRL_DC_L0_EN_OFFSET) |
                 (1 << ARCV_MCACHE_CTRL_L2_EN_OFFSET);
-        __asm__ __volatile__("csrw %0, %1" :: "i"(CSR_NUM_ARCV_MCACHE_CTRL), "r"(mcache));
+        _arcv_csr_write(CSR_NUM_ARCV_MCACHE_CTRL, mcache);
+}
+
+static void __section(".init") __used __attribute__((target("arch=+zicsr")))
+_arcv_vector_enable()
+{
+        unsigned long rvv_build = _arcv_csr_read(CSR_NUM_ARCV_RVV_BUILD);
+        unsigned long v_option = (rvv_build >> ARCV_RVV_BUILD_V_OPTION_OFFSET) &
+                ARCV_RVV_BUILD_V_OPTION_MASK;
+
+        if (v_option != 0)
+        {
+            unsigned long mstatus = _arcv_csr_read(CSR_NUM_MSTATUS);
+            mstatus |= (1 << MSTATUS_VS_OFFSET);
+            _arcv_csr_write(CSR_NUM_MSTATUS, mstatus);
+        }
 }
 #endif
 
@@ -229,24 +265,9 @@ _start(void)
         __asm__("csrr   t1, mtvec");
 #endif
 
-#ifndef CRT0_NO_CSR
-        /**
-         * Check misa.V (misa[21]) for V extension presence.
-         * Set mstatus.VS (mstatus[10:9]) if V extension is present.
-         */
-        __asm__("csrr   t0, misa");
-        __asm__("srli   t0, t0, 21");
-        __asm__("andi   t0, t0, 1");
-        __asm__("beqz   t0, 1f");
-        __asm__("csrr   t0, mstatus");
-        __asm__("li     t1, 512");
-        __asm__("or     t0, t1, t0");
-        __asm__("csrw   mstatus, t0");
-        __asm__("1:");
-#endif
-
 #if defined (CRT0_ARCV) && !defined(CRT0_NO_CSR)
         __asm__("jal   _arcv_cache_enable");
+        __asm__("jal   _arcv_vector_enable");
 #endif
 
         __asm__("j      _cstart");
