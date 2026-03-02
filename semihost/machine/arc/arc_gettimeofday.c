86412e27230faf08044ac78f26551d203a24c41e
Yuriy Kolerov <ykolerov@synopsys.com>
2026-03-02 09:05:16 +0000

    fixup! semihost/arc: Add support of gettimeofday system call

--- semihost/machine/arc/arc_gettimeofday.c
+++ semihost/machine/arc/arc_gettimeofday.c
@@ -3,9 +3,9 @@
 #include <sys/types.h>
 
 int
-gettimeofday(struct timeval * restrict tv, void * restrict tz)
+gettimeofday(struct timeval *tv, void *tz)
 {
-    int ret = arc_semihost2(SYS_SEMIHOST_gettimeofday, tv, tz);
+    int ret = arc_semihost2(SYS_SEMIHOST_gettimeofday, (uintptr_t)tv, (uintptr_t)tz);
     if (ret == -1)
         arc_semihost_errno(EFAULT);
     return ret;
